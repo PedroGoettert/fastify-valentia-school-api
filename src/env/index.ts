b18d5791fb96c365d094a0e1229e0cf01b68890b
Pedro de Souza Goettert <pedrogoettert@hotmail.com>
2025-04-23 08:49:51 -0300

    add node env variable for deploy and adjust biome config

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,7 @@ import z from "zod";
 const envSchema = z.object({
 	PORT: z.coerce.number(),
 	DATABASE_URL: z.string(),
+	NODE_ENV: z.enum(["DEV", "PRODUCTION", "TEST"]).default("PRODUCTION"),
 });
 
 export const env = envSchema.parse(process.env);
