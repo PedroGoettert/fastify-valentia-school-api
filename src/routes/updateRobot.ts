1e7c1e5044aa99aa2f821e7a42671e7c1fee14b5
Pedro de Souza Goettert <pedrogoettert@hotmail.com>
2025-04-25 07:41:09 -0300

    feat: add /ping route for uptime monitoring

---
+++ src/routes/updateRobot.ts
@@ -0,0 +1,7 @@
+import type { FastifyInstance } from "fastify";
+
+export async function UpdateRoboot(server: FastifyInstance) {
+	server.get("/ping", async (request, reply) => {
+		return reply.status(200);
+	});
+}
